8bce86c34c9175f917757fa688801bae83a821cb
rasta <aasm.prog@gmail.com>
2025-05-18 15:18:39 -0300

    asc e desc

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,9 @@ function App() {
   
   const [search, setSearch] = useState("")
 
+  const [filter, setFilter] = useState("All")
+  const [sort, setSort]= useState("Asc")
+
   const addTodo = (text, category) =>{
     const newTodos = [...todos,{
       id: Math.floor(Math.random()*10000),
@@ -56,11 +59,22 @@ function App() {
   return  <div className="app">
     <h1>Lista de tarefas</h1>
     <Search search={search} setSearch={setSearch} />
-    <Filter/>
+    <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
     <div className="todo-list">
       {todos 
-      .filter((todo)=> todo.text.toLocaleLowerCase()
-      .includes(search.toLocaleLowerCase()))
+      .filter((todo) => 
+        filter ==="All" 
+        ? true 
+        : filter === "Completed" 
+        ? todo.isCompleted 
+        : !todo.isCompleted )
+      .filter((todo)=> 
+        todo.text.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+      .sort((a,b)=> 
+        sort === "Asc"
+        ? a.text.localeCompare(b.text)
+        : b.text.localeCompare(a.text)
+        )
       .map((todo) =>(
         <Todo 
         key={todo.id} 
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,23 +1,25 @@
-const Filter = () => {
-  return (
-    <div className="filter">
-        <h2>Filtrar:</h2>
-        <div className="filter-options">
-            <div>
-                <p>Status:</p>
-                <select>
-                    <option value="All">Todas</option>
-                    <option value="Completed">Completas</option>
-                    <option value="Incompleted">Incompletas</option>
-                </select>
+const Filter = ({filter, setFilter, setSort}) => {
+    return (
+        <div className="filter">
+            <h2>Filtrar:</h2>
+            <div className="filter-options">
+                <div>
+                    <p>Status:</p>
+                    <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+                        <option value="All">Todas</option>
+                        <option value="Completed">Completas</option>
+                        <option value="Incompleted">Incompletas</option>
+                    </select>
+                </div>
+                <div>
+                    <p>Ordem alfabética:</p>
+                    <button onClick={() =>setSort("Asc")}>Asc</button>
+                    <button onClick={() =>setSort("Desc")}>Desc</button>
+                </div>
             </div>
-            <p>Ordem alfabética:</p>
-            <button>Asc</button>
-            <button>Desc</button>
-        </div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Filter
